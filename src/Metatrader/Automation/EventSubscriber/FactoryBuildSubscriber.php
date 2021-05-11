86bc44bf382718f409c5ff63d4279f100042d340
Alpha <alpha@pulsarcode.com>
2021-05-11 19:00:35 +0200

    Use form component to build the entity and validate on the fly

--- src/Metatrader/Automation/EventSubscriber/FactoryBuildSubscriber.php
+++ src/Metatrader/Automation/EventSubscriber/FactoryBuildSubscriber.php
@@ -5,35 +5,44 @@ declare(strict_types=1);
 namespace App\Metatrader\Automation\EventSubscriber;
 
 use App\Metatrader\Automation\Annotation\Subscriber;
-use App\Metatrader\Automation\Domain\Backtest;
-use App\Metatrader\Automation\Event\FactoryBuildBacktestEvent;
+use App\Metatrader\Automation\Event\Entity\AbstractBuildEntityEvent;
+use App\Metatrader\Automation\Event\Entity\FactoryBuildBacktestEntityEvent;
+use App\Metatrader\Automation\Event\Entity\FactoryBuildBacktestReportEntityEvent;
 use App\Metatrader\Automation\ExpertAdvisor\AbstractExpertAdvisor;
 use App\Metatrader\Automation\ExpertAdvisor\ExpertAdvisorParameters;
+use App\Metatrader\Automation\Form\Type\BacktestReportType;
+use App\Metatrader\Automation\Form\Type\BacktestType;
 use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
 
 /**
  * @Subscriber
  */
 class FactoryBuildSubscriber
 {
+    private FormFactoryInterface  $formFactory;
     private ContainerBagInterface $parameters;
 
-    public function __construct(ContainerBagInterface $parameters)
+    public function __construct(ContainerBagInterface $parameters, FormFactoryInterface $formFactory)
     {
-        $this->parameters = $parameters;
+        $this->parameters  = $parameters;
+        $this->formFactory = $formFactory;
     }
 
-    public function onBacktestEvent(FactoryBuildBacktestEvent $event): void
+    public function onBuildBacktestEntityEvent(FactoryBuildBacktestEntityEvent $event): void
     {
-        $model    = $event->getModel();
-        $backtest = new Backtest();
-        $backtest->setExpertAdvisor($this->getExpertAdvisorInstance($model->getName()));
-        $backtest->setSymbol($model->getSymbol());
-        $backtest->setPeriod($model->getPeriod());
-        $backtest->setDeposit($model->getDeposit());
-        $backtest->setFrom($model->getFrom());
-        $backtest->setTo($model->getTo());
-        $event->setBacktest($backtest);
+        $this->handleFormType(BacktestType::class, $event, 'setBacktestEntity');
+    }
+
+    public function onBuildBacktestReportEntityEvent(FactoryBuildBacktestReportEntityEvent $event): void
+    {
+        $this->handleFormType(BacktestReportType::class, $event, 'setBacktestReportEntity');
+    }
+
+    private function buildForm(string $formType): FormInterface
+    {
+        return $this->formFactory->createBuilder($formType)->getForm();
     }
 
     private function getExpertAdvisorInstance(string $name): AbstractExpertAdvisor
@@ -42,5 +51,23 @@ class FactoryBuildSubscriber
         $parameters = new ExpertAdvisorParameters($this->parameters->get('expert_advisors')[$name] ?? []);
 
         return new $class($name, $parameters);
+    }
+
+    private function handleFormType(string $formType, AbstractBuildEntityEvent $event, string $callback): void
+    {
+        $form = $this->buildForm($formType);
+        $form->submit($event->getParameters());
+
+        if (!$form->isValid())
+        {
+            foreach ($form->getErrors() as $formError)
+            {
+                $event->addError($formError->getMessage());
+            }
+
+            return;
+        }
+
+        call_user_func([$event, $callback], $form->getData());
     }
 }

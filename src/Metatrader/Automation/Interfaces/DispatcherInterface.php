6f9599ab9576d5233cb09b121b490c716d5ea49a
Alpha <alpha@pulsarcode.com>
2021-05-11 18:57:55 +0200

    Use interface, not implementation

--- src/Metatrader/Automation/Interfaces/DispatcherInterface.php
+++ src/Metatrader/Automation/Interfaces/DispatcherInterface.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Metatrader\Automation\Interfaces;
 
-use App\Metatrader\Automation\Event\AbstractEvent;
+use App\Metatrader\Automation\Event\EventInterface;
 
 interface DispatcherInterface
 {
-    public function dispatch(AbstractEvent $event): object;
+    public function dispatch(EventInterface $event): object;
 }

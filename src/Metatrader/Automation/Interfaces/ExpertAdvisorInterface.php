88dc455788e836b3d916dfd23b28f8c3cbaadf04
Alpha <alpha@pulsarcode.com>
2021-05-13 19:18:03 +0200

    Missing interface

---
+++ src/Metatrader/Automation/Interfaces/ExpertAdvisorInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Metatrader\Automation\Interfaces;
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+interface ExpertAdvisorInterface
+{
+    public static function getExpertAdvisorClass(string $expertAdvisorName): string;
+
+    public function getBacktestGenerator(EntityInterface $backtestEntity): \Generator;
+
+    public function getName(): string;
+
+    public function getParameters(): ParameterBag;
+
+    public function isActive(): bool;
+}

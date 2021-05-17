c7bde1825820b54791c5d94e2354d7d42af49af1
Alpha <alpha@pulsarcode.com>
2021-05-18 01:12:38 +0200

    More migrations

---
+++ migrations/Version20210517154316.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210517154316 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE backtest_entity ADD expert_advisor VARCHAR(255) NOT NULL, ADD last_backtest_report VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE backtest_entity DROP expert_advisor, DROP last_backtest_report');
+    }
+}

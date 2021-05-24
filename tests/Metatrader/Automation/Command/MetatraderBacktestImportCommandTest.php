1e27966ece16313e45ce4c626b0af5dcbeed47e9
Alpha <alpha@pulsarcode.com>
2021-05-24 14:35:21 +0200

    New partial test

---
+++ tests/Metatrader/Automation/Command/MetatraderBacktestImportCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Metatrader\Automation\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class MetatraderBacktestImportCommandTest extends KernelTestCase
+{
+    public function getExecuteData(): array
+    {
+        return [
+            [
+                [
+                    'directory' => __DIR__,
+                ],
+                '[ERROR] The directory ' . __DIR__ . ' does not contains any backtest report',
+            ],
+            [
+                [
+                    'directory' => __DIR__ . 'a1b2c3d4e5f6',
+                ],
+                '[ERROR] The "' . __DIR__ . 'a1b2c3d4e5f6" directory does not exist.',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getExecuteData
+     */
+    public function testExecute(array $arguments, string $expected): void
+    {
+        $kernel        = static::createKernel();
+        $application   = new Application($kernel);
+        $command       = $application->find('metatrader:backtest:import');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute($arguments);
+
+        static::assertStringContainsString($expected, preg_replace('/^\s+|\s+$|\s+(?=\s)/', '', $commandTester->getDisplay()));
+    }
+}

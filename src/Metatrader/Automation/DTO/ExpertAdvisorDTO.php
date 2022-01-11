34f0333b968e685c0e92b53ec199e47a7b4fc4b1
Alpha <alpha@pulsarcode.com>
2022-01-11 15:38:44 +0100

    Implement a empty Expert Advisor DTO

---
+++ src/Metatrader/Automation/DTO/ExpertAdvisorDTO.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Metatrader\Automation\DTO;
+
+use Spatie\DataTransferObject\DataTransferObject as DTO;
+
+class ExpertAdvisorDTO extends DTO
+{
+}

3c8de8ff31e53de666b64dff602e36ae1e1c5149
Alpha <alpha@pulsarcode.com>
2022-01-11 13:18:10 +0100

    Argument 2 passed to Doctrine\ORM\Mapping\UniqueConstraint::__construct() must be of the type array or null, string given

--- src/Metatrader/Automation/Entity/BacktestEntity.php
+++ src/Metatrader/Automation/Entity/BacktestEntity.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BacktestEntityRepository::class)
- * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(columns="name")})
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(columns={"name"})})
  */
 class BacktestEntity extends AbstractEntity
 {

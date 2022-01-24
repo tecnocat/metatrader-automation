832817f8faa323a66e45aa9b3002154c1b4dcbec
Alpha <alpha@pulsarcode.com>
2022-01-24 23:29:54 +0100

    Remove unnecessary stuff and implement the new DTOs

--- src/Metatrader/Automation/Interfaces/ExpertAdvisorInterface.php
+++ src/Metatrader/Automation/Interfaces/ExpertAdvisorInterface.php
@@ -4,23 +4,21 @@ declare(strict_types=1);
 
 namespace App\Metatrader\Automation\Interfaces;
 
+use App\Metatrader\Automation\DTO\BacktestDTO;
+use App\Metatrader\Automation\DTO\BacktestExecutionDTO;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
 interface ExpertAdvisorInterface
 {
     public static function getExpertAdvisorClass(string $expertAdvisorName): string;
 
-    public function generateBacktestReportName(EntityInterface $backtestEntity): \Generator;
+    public function getBacktestExecutionDTO(BacktestDTO $backtestDTO, array $iteration): BacktestExecutionDTO;
 
-    public function getCurrentBacktestSettings(): array;
+    public function getIteration(BacktestDTO $backtestDTO): \Generator;
 
     public function getName(): string;
 
     public function getParameters(): ParameterBag;
 
     public function isActive(): bool;
-
-    public function setCurrentBacktestSettings(array $currentBacktestSettings): void;
-
-    public function getAlias(): array;
 }

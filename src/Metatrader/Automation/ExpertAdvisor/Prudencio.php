92725cc0ddec1f49587af635795028a0c89428e9
Alpha <alpha@pulsarcode.com>
2022-01-24 23:20:14 +0100

    Simplify to the minimum expression, now only need to do some custom parameters instead a lot of sick and ugly stuff, remove alias and use original Expert Advisor parameters from your EA.ex4

--- src/Metatrader/Automation/ExpertAdvisor/Prudencio.php
+++ src/Metatrader/Automation/ExpertAdvisor/Prudencio.php
@@ -4,42 +4,19 @@ declare(strict_types=1);
 
 namespace App\Metatrader\Automation\ExpertAdvisor;
 
-use App\Metatrader\Automation\Interfaces\EntityInterface;
+use App\Metatrader\Automation\DTO\BacktestDTO;
+use App\Metatrader\Automation\Helper\BacktestReportHelper;
 
 class Prudencio extends AbstractExpertAdvisor
 {
-    public function generateBacktestReportName(EntityInterface $backtestEntity): \Generator
+    public function getIteration(BacktestDTO $backtestDTO): \Generator
     {
-        $iterations = [
-            // TODO: How to fix 'Cannot traverse an already closed generator' ?
-            // TODO: Try https://github.com/PatchRanger/cartesian-iterator
-            iterator_to_array($this->dateRangeIterator($backtestEntity->getFrom(), $backtestEntity->getTo(), $this->getParameters()->getInt('step_months'))),
-            iterator_to_array($this->minMaxIterator('distance', $this->getParameters()->get('distance'))),
-            iterator_to_array($this->minMaxIterator('hedging', $this->getParameters()->get('hedging'))),
-            iterator_to_array($this->minMaxIterator('profit', $this->getParameters()->get('profit'))),
-            iterator_to_array($this->simpleIterator('period', [$backtestEntity->getPeriod()])),
-            iterator_to_array($this->simpleIterator('increment', [0])),
-        ];
-
-        foreach ($this->iterate($iterations) as $iteration)
+        foreach ($this->loadIterations($backtestDTO) as $iteration)
         {
-            $iteration['loss'] = $iteration['hedging'] * 2;
+            $prefix                         = BacktestReportHelper::INPUTS_PARAMETER_PREFIX;
+            $iteration[$prefix . 'Perdida'] = $iteration[$prefix . 'Cobertura'] * 2;
 
-            yield $this->getBacktestReportName($iteration);
+            yield $iteration;
         }
-    }
-
-    public function getAlias(): array
-    {
-        return [
-            // PHP name    MT4 EA name (.ex4)
-            'logLevel'  => 'LogLevel',
-            'distance'  => 'Distancia',
-            'hedging'   => 'Cobertura',
-            'loss'      => 'Perdida',
-            'profit'    => 'Beneficio',
-            'increment' => 'Exponencial',
-            'multiply'  => 'Multiplicador',
-        ];
     }
 }

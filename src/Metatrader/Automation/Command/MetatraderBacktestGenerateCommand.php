deafae70376c4251ffacfe4c925a08350a0374be
Alpha <alpha@pulsarcode.com>
2022-01-24 23:27:27 +0100

    Use DTO instead entity and rename some fields to match original names in backtest reports

--- src/Metatrader/Automation/Command/MetatraderBacktestGenerateCommand.php
+++ src/Metatrader/Automation/Command/MetatraderBacktestGenerateCommand.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace App\Metatrader\Automation\Command;
 
-use App\Metatrader\Automation\Entity\BacktestEntity;
-use App\Metatrader\Automation\Event\Entity\BuildEntityEvent;
+use App\Metatrader\Automation\DTO\BacktestDTO;
 use App\Metatrader\Automation\Event\Metatrader\ExecutionEvent;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -14,32 +13,22 @@ class MetatraderBacktestGenerateCommand extends AbstractCommand
 {
     public function process(): int
     {
-        $parameters       = array_merge($this->getArguments(), $this->getOptions());
-        $buildEntityEvent = new BuildEntityEvent(BacktestEntity::class, $parameters);
-        $this->dispatch($buildEntityEvent);
-
-        if ($this->hasErrors($buildEntityEvent))
-        {
-            return Command::FAILURE;
-        }
-
-        /** @var BacktestEntity $entity */
-        $entity  = $buildEntityEvent->getEntity();
-        $headers = ['Expert Advisor', 'Symbol', 'Period', 'Deposit', 'From', 'To'];
-        $rows    = [
+        $backtestDTO = new BacktestDTO(array_merge($this->getArguments(), $this->getOptions()));
+        $headers     = ['Expert Advisor Name', 'Symbol', 'Period', 'From', 'To', 'Initial Deposit'];
+        $rows        = [
             [
-                $entity->getExpertAdvisor(),
-                $entity->getSymbol(),
-                $entity->getPeriod(),
-                $entity->getDeposit(),
-                $entity->getFrom()->format('Y-m-d'),
-                $entity->getTo()->format('Y-m-d'),
+                $backtestDTO->expertAdvisorName,
+                $backtestDTO->symbol,
+                $backtestDTO->period,
+                $backtestDTO->from->format('Y-m-d'),
+                $backtestDTO->to->format('Y-m-d'),
+                $backtestDTO->initialDeposit,
             ],
         ];
         $this->comment('Executing Metatrader Automation...');
         $this->table($headers, $rows);
 
-        $executionEvent = new ExecutionEvent($entity);
+        $executionEvent = new ExecutionEvent($backtestDTO);
         $this->dispatch($executionEvent);
 
         if ($this->hasErrors($executionEvent))
@@ -55,10 +44,10 @@ class MetatraderBacktestGenerateCommand extends AbstractCommand
         parent::configure();
 
         $this
-            ->addArgument('expertAdvisor', InputArgument::REQUIRED, 'The name of the Expert Advisor')
+            ->addArgument('expertAdvisorName', InputArgument::REQUIRED, 'The name of the Expert Advisor')
             ->addArgument('symbol', InputArgument::REQUIRED, 'The symbol to test with the Expert Advisor')
             ->addArgument('period', InputArgument::REQUIRED, 'The period to test with the Expert Advisor')
-            ->addArgument('deposit', InputArgument::REQUIRED, 'The amount of equity to test with the Expert Advisor')
+            ->addArgument('initialDeposit', InputArgument::REQUIRED, 'The amount of equity to test with the Expert Advisor')
             ->addArgument('from', InputArgument::REQUIRED, 'The from date to test with the Expert Advisor')
             ->addArgument('to', InputArgument::REQUIRED, 'The to date to test with the Expert Advisor')
             ->setDescription('Generate a Metatrader backtest reports based on selected parameters')
